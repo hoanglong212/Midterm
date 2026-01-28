0dbabb829b24c509e08ac9dca9f85cd725005613
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-01-28 13:46:57 +0700

    web

--- react-tailwind/vite.config.js
+++ react-tailwind/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Midterm/', // 👈 BẮT BUỘC
   plugins: [
     react({
       babel: {
